d0feb15c08992eaecbcbfdc3ae31846a1a853305
xukaijie2022@126.com <xukaijie2022@126.com>
2023-06-01 17:52:35 +0800

    runtime

---
+++ packages/runtime-base/src/Component.ts
@@ -0,0 +1,35 @@
+
+import {
+    LIFEKEYS
+} from './types'
+
+export class BaseComponent {
+
+    data:Record<string,any>
+    methods:Record<string,Function>
+    lifeTimes:Map<LIFEKEYS,Function>
+
+    constructor() {
+        this.data = {}
+        this.methods = {}
+        this.lifeTimes = new Map();
+    }
+
+
+    init() {
+        
+
+    }
+
+
+    callMethod(name:string) {
+
+
+    }
+
+    callLifeTimes(name:string) {
+
+
+    }
+
+}
---
+++ packages/runtime-base/src/types.ts
@@ -0,0 +1,4 @@
+
+
+
+export type LIFEKEYS = "onCreated" | "onShow" | "onReady" | "onDestroyed"
