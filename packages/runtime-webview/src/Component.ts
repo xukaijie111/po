0ff873ede29eddbd96a2928bf6169a99fb9d5289
xukaijie2022@126.com <xukaijie2022@126.com>
2023-06-13 17:03:53 +0800

    component

--- packages/runtime-webview/src/Component.ts
+++ packages/runtime-webview/src/Component.ts
@@ -6,6 +6,7 @@ import {
 
 
 import type {
+    Webview,
     ExposeComponentOptions
 } from './Webview'
 
@@ -13,37 +14,116 @@ import {
     generateMixed
 } from '@po/shared'
 
-export class Component extends BaseComponent{
+import {
+    BRIDGE_DOM_ON_CLICK_DATA,
+    PROTOCOL_CMD
+} from '@po/bridge-client'
 
-    parent:Component
-    children:Array<Component>
-    constructor(private options:ExposeComponentOptions,props:Record<string,any>){
+import {
+    amountElement, patch
+} from './patch'
+
+import { VNode  , pushCurrentComponent,popCurrentComponent} from "./node";
+
+
+export class Component extends BaseComponent {
+
+    parent: Component
+    children: Array<Component>
+    webview: Webview
+    cache: Record<string, any>
+    vnode:VNode
+    prevVnode:VNode
+    constructor(private options: ExposeComponentOptions, props: Record<string, any>) {
         super()
         this.props = props
         this.id = generateMixed()
+        //@ts-ignore
+        this.webview = window.webview
+        this.cache = {};// 缓存DOM事件
 
     }
 
-    init() {
-
-    }
+    // 执行组件生命周期，创建vnode ,这里还没开始挂载
+    async init() {
+        await this.callHookCreate();
+        this.render();
+     }
 
     callLifeTimes(name: string): void {
-        
+
     }
 
+    // 执行组件生命周期created/show
+    async callHookCreate() {
+        let { webview,options,id,props } = this;
+        let { bridge } = webview
+
+        this.data = await bridge.send({
+            type: PROTOCOL_CMD.C2S_INIT_COMPONENT,
+            data: {
+                name: options.name,
+                componentId:id,
+                parentId:this.parent?.id,
+                props
+            }
+        })
+
+        return ;
+    }
 
     callMethod(name: string): void {
-        
-    }
-
-
-    amount(elm:Node,refElm:Node = null) {
 
     }
 
 
-    addChildren(child:Component){
+    amount(elm: Node, refElm: Node = null) {
+        return  amountElement(this.vnode,elm,refElm)
+    }
+
+    render() {
+        let { options,data } = this;
+        let { render } = options;
+
+        pushCurrentComponent(this)
+        let vnode = render(data)
+        popCurrentComponent()
+        this.prevVnode = this.vnode;
+        this.vnode = vnode
+    }
+
+
+    // 给render 函数调用
+    getOn(name: string) {
+        let { cache, webview, id } = this;
+        if (cache[name]) return cache[name]
+
+        cache[name] = function (event: Event) {
+            let data: BRIDGE_DOM_ON_CLICK_DATA = {
+                type: PROTOCOL_CMD.C2S_DOM_ON_CLICK,
+                data: {
+                    componentId: id,
+                    name,
+                    params: "will to do"
+                }
+            }
+            webview.send(data)
+
+            event.stopPropagation();
+        }
+
+        return cache[name]
+    }
+
+
+    async update(data) {
+        this.data = data;
+        this.render();
+        patch(this.prevVnode,this.vnode)
+    }
+
+
+    addChildren(child: Component) {
         this.children.push(child)
     }
 }
